b073fd5665d505dbcfabea88ad2efc347634e943
Anthony-Chela <chelaanthony@gmail.com>
2020-08-24 14:57:43 +0300

    add new variables

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
-function akanData(params) {
+$(document).ready(function(){
+  $("#yourAkanName").mask("dateofBirth");
+});
 
-var birthDate = document.getElementById("birthDate").value;
-var birthYear = document.getElementById("birthYear").value;
-var birthMonth = document.getElementById("birthMonth").value;
-}
+  var birthDate = document.getElementById("birthDate").value;
+  var birthYear = document.getElementById("birthYear").value;
+  var birthMonth = document.getElementById("birthMonth").value;
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+  var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
 
 var birthDateno = parseInt("birthDate");
-if (birthDateno > 31){
+
+if (birthDateno > 31 || birthDateno < 0){
   console.log("Invalid date please re-enter");
 }
+
+
+
+
