d2ea1149c133c08f0c74f2a83d8cc48791ce9463
Anthony-Chela <chelaanthony@gmail.com>
2020-08-25 08:51:09 +0300

    add the control flows

--- scripts.js
+++ scripts.js
@@ -1,30 +1,18 @@
-function akan(){
-  var birthDate = document.getElementById("birthDate").value;
-  var birthYear = document.getElementById("birthYear").value;
-  var birthMonth = document.getElementById("birthMonth").value;
-  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-  var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
+  var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+  var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// to obtain numeric data
 
-var birthDate = parseInt("birthDate")
+  function genName(params) {
+        var year = parseInt(document.getElementById("birthYear").value);
+	      var month = parseInt(document.getElementById("birthMonth").value);
+	      var date = parseInt(document.getElementById("birthDate").value);
+        var male = document.getElementById("gender");
+	      var female = document.getElementById("gender");
+  // to validate month and date 
 
-var birthYear = parseInt("birthYear")
-
-var birthMonth = parseInt("birthMonth")
-
-
-if (birthDate> 31 || birthDate < 0){
-  console.log("Invalid date please re-enter");
-}
-
-var birthMonth = parseInt("birthMonth");
-
-if (birthMonth > 12 || birthMonth <0){
-  console.log("Invalid month please re-enter")
-}
-}
-
-
-
-
+  if (date < 0 || > 32) {
+    alert ( "Invalid date. please re-enter the date of birth" )
+  } 
+  
